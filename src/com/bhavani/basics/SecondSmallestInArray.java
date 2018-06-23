ac86b77129a7d78ebd507a4139b0c35dac240bc5
bhavani <bhavaniswamyvelu8@gmail.com>
2018-06-23 09:00:24 +0530

    Logical Programs

---
+++ src/com/bhavani/basics/SecondSmallestInArray.java
@@ -0,0 +1,25 @@
+package com.bhavani.basics;
+
+public class SecondSmallestInArray {
+	public static int getSmallest(int[] a, int total) {
+		int temp;
+		for (int i = 0; i < total; i++) {
+			for (int j = i + 1; j < total; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		return a[1];
+	}
+
+	public static void main(String args[]) {
+		int a[] = { 1, 2, 5, 6, 3, 2 };
+		int b[] = { 44, 66, 99, 77, 33, 22, 55 };
+		System.out.println(" Second Smallest: " + getSmallest(a, 6));
+		System.out.println(" Second Smallest: " + getSmallest(b, 7));
+	}
+
+}
